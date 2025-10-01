8253a7008ec18f2e00da0057fd5f791123e3d2bc
Lazarev Artem <lazarievartem25@gmail.com>
2025-10-01 20:21:28 +0300

    end?

--- js/hero.js
+++ js/hero.js
@@ -6,28 +6,28 @@ const team4 = document.getElementById("team4")
 function changeImg() {
     if (window.devicePixelRatio > 1) {
         if (window.innerWidth >= 756) {
-            team1.src = "../img/team1-d.webp"
-            team2.src = "../img/team2-d.webp"
-            team3.src = "../img/team3-d.webp"
-            team4.src = "../img/team4-d.webp"
+            team1.src = "img/team1-d.webp"
+            team2.src = "img/team2-d.webp"
+            team3.src = "img/team3-d.webp"
+            team4.src = "img/team4-d.webp"
         }
         if (window.innerWidth < 756 && window.innerWidth >= 480) {
-            team1.src = "../img/team1-t.webp"
-            team2.src = "../img/team2-t.webp"
-            team3.src = "../img/team3-t.webp"
-            team4.src = "../img/team4-t.webp"
+            team1.src = "img/team1-t.webp"
+            team2.src = "img/team2-t.webp"
+            team3.src = "img/team3-t.webp"
+            team4.src = "img/team4-t.webp"
         }
         if (window.innerWidth < 480) {
-            team1.src = "../img/team1.webp"
-            team2.src = "../img/team2.webp"
-            team3.src = "../img/team3.webp"
-            team4.src = "../img/team4.webp"
+            team1.src = "img/team1.webp"
+            team2.src = "img/team2.webp"
+            team3.src = "img/team3.webp"
+            team4.src = "img/team4.webp"
         }
     } else {
-        team1.src = '../img/igor.jpg'
-        team2.src = '../img/olha.jpg'
-        team3.src = '../img/mukola.jpg'
-        team4.src = '../img/michael.jpg'
+        team1.src = 'img/igor.jpg'
+        team2.src = 'img/olha.jpg'
+        team3.src = 'img/mukola.jpg'
+        team4.src = 'img/michael.jpg'
     }
 }
 changeImg()
