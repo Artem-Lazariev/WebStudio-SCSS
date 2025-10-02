8c1bf4ea9f6cb1df87808d7f6d3d4784e6ef4662
Lazarev Artem <lazarievartem25@gmail.com>
2025-10-02 21:04:03 +0300

    test

--- js/hero.js
+++ js/hero.js
@@ -1,34 +1,65 @@
-const team1 = document.getElementById("team1")
-const team2 = document.getElementById("team2")
-const team3 = document.getElementById("team3")
-const team4 = document.getElementById("team4")
+// Отримуємо всі елементи
+const teams = [
+    document.getElementById("team1"),
+    document.getElementById("team2"),
+    document.getElementById("team3"),
+    document.getElementById("team4")
+];
 
-function changeImg() {
+const works = [
+    document.getElementById("work1"),
+    document.getElementById("work2"),
+    document.getElementById("work3")
+];
+
+// Функція для зміни зображень команд
+function changeTeamImgs() {
     if (window.devicePixelRatio > 1) {
         if (window.innerWidth >= 756) {
-            team1.src = "img/team1-d.webp"
-            team2.src = "img/team2-d.webp"
-            team3.src = "img/team3-d.webp"
-            team4.src = "img/team4-d.webp"
-        }
-        if (window.innerWidth < 756 && window.innerWidth >= 480) {
-            team1.src = "img/team1-t.webp"
-            team2.src = "img/team2-t.webp"
-            team3.src = "img/team3-t.webp"
-            team4.src = "img/team4-t.webp"
-        }
-        if (window.innerWidth < 480) {
-            team1.src = "img/team1.webp"
-            team2.src = "img/team2.webp"
-            team3.src = "img/team3.webp"
-            team4.src = "img/team4.webp"
+            teams[0].src = "img/team1-d.webp";
+            teams[1].src = "img/team2-d.webp";
+            teams[2].src = "img/team3-d.webp";
+            teams[3].src = "img/team4-d.webp";
+        } else if (window.innerWidth >= 480) {
+            teams[0].src = "img/team1-t.webp";
+            teams[1].src = "img/team2-t.webp";
+            teams[2].src = "img/team3-t.webp";
+            teams[3].src = "img/team4-t.webp";
+        } else {
+            teams[0].src = "img/team1.webp";
+            teams[1].src = "img/team2.webp";
+            teams[2].src = "img/team3.webp";
+            teams[3].src = "img/team4.webp";
         }
     } else {
-        team1.src = 'img/igor.jpg'
-        team2.src = 'img/olha.jpg'
-        team3.src = 'img/mukola.jpg'
-        team4.src = 'img/michael.jpg'
+        teams[0].src = 'img/igor.jpg';
+        teams[1].src = 'img/olha.jpg';
+        teams[2].src = 'img/mukola.jpg';
+        teams[3].src = 'img/michael.jpg';
     }
 }
-changeImg()
-window.addEventListener("resize", changeImg)
+
+// Функція для зміни зображень робіт
+function changeWorkImgs() {
+    if (window.devicePixelRatio > 1) {
+        works[0].src = "./img/work1.webp";
+        works[1].src = "./img/work2.webp";
+        works[2].src = "./img/work3.webp";
+    } else {
+        works[0].src = "./img/comp.jpg";
+        works[1].src = "./img/tel.jpg";
+        works[2].src = "./img/tablet.jpg";
+    }
+}
+
+// Загальна функція
+function updateImgs() {
+    changeTeamImgs();
+    changeWorkImgs();
+}
+
+// Виклик одразу
+updateImgs();
+
+// Виклик при зміні розміру вікна
+window.addEventListener("resize", updateImgs);
